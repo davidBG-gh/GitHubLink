420f1bdca458256d440b4dc0842e6671002c94eb
david BG <david.bengr@gmail.com>
2019-11-12 19:35:37 +0200

    2 entries from rstudio

--- script1.R
+++ script1.R
@@ -1,4 +1,6 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
 print("Entered in Github Nov 12th")
+print("Entered in Rstudio Nov 12th")
+print("Entry 2 in Rstudio Nov 12th")
 
